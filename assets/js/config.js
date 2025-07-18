a8bc12a37ce60346ce2759ee725bf1a2fa91117b
Muhye AlDeen Akram Al-Raie <86913045+MuhyeAlRaie@users.noreply.github.com>
2025-07-18 05:17:28 +0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,10 +4,10 @@ const CONFIG = {
   CURRENCY: "د.أ",
   
   // رابط Google Apps Script لقراءة المنتجات من Google Sheet
-  PRODUCTS_API_URL: "https://script.google.com/macros/s/AKfycbwzLk31B9W5GYm1Tmk1Ht7eDrQiBLxaMEzqq7pxxjBziHhK16H9VZhyYfGwwHPVHE8NBA/exec",
+  PRODUCTS_API_URL: "https://script.google.com/macros/s/AKfycbyWjt64bxMAJIoaV1FbAQoYEU0wHRFJ7EUFGgBq4YNWqBWKmAQFBTHCDDCBoLLla40gnQ/exec",
 
   // رابط Google Apps Script لتخزين الطلبات في Google Sheet
-  ORDERS_API_URL: "https://script.google.com/macros/s/AKfycbxXWpfQgwH3GT-LuqJgdudHs0Y0MCLzWaylhKdE3oYC2-1cmYoN1VVaoGqPZUzKul54/exec",
+  ORDERS_API_URL: "https://script.google.com/macros/s/AKfycbzX6MUglLN46AlqD99QZ7LY31e3IWFx8BopRcfPewX0mO7gbHPzsFG-yTzVRQUqmGlH/exec",
 
   // رسوم التوصيل حسب المناطق (تعديل حسب الحاجة)
   DELIVERY_FEES: {
