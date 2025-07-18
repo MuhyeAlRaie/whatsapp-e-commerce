0fd8d24858a5e824e188e4dde89f2be7518e608a
Muhye AlDeen Akram Al-Raie <86913045+MuhyeAlRaie@users.noreply.github.com>
2025-07-18 07:25:06 +0300

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,7 +4,7 @@ const CONFIG = {
   CURRENCY: "د.أ",
   
   // رابط Google Apps Script لقراءة المنتجات من Google Sheet
-  PRODUCTS_API_URL: "https://script.google.com/macros/s/AKfycbxn4D8F8pC0bsR_pNNOHntfWZaVpH78priC_jW-GvkQ-Ja3_ILUkE5mGx8Dv8ZS4u7rEw/exec",
+  PRODUCTS_API_URL: "https://script.google.com/macros/s/AKfycbyQ1eG5EynTHOpxJNFQBVJiAU5QTX880Y-jctfM1GS1UVkpOXOFZ_SEqI1GTAQ87UUPhA/exec",
 
   // رابط Google Apps Script لتخزين الطلبات في Google Sheet
   ORDERS_API_URL: "https://script.google.com/macros/s/AKfycbw-pT4ThTSIDbAlBfe-r2Q6rIkW9LFYRqWhLqlC8ExvJEb9l0V_WIlR--9F4ze2_ycn/exec",
